241dd545614d44c824855b6daf8b6e71c9016f5c
grow-lesson <grow.lesson.info@gmail.com>
2023-07-04 19:10:14 +0900

    ユーザー情報のルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, skip: [:sessions]
 
   namespace :api, defaults: { format: :json } do
+    resources :users, only: [:index]
     devise_scope :user do
       post '/login', to: 'sessions/sessions#create'
     end
