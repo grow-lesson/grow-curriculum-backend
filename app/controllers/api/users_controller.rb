d673854f0b38072d681711f35660320e86d1a5fe
grow-lesson <grow.lesson.info@gmail.com>
2023-08-10 18:04:07 +0900

    新規登録画面とログイン画面の機能実装

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,9 +1,11 @@
 class Api::UsersController < ApplicationController
-    def index
-      users = User.all
-      response.headers['Cache-Control'] = 'no-cache, no-store'
-      response.headers['Pragma'] = 'no-cache'
-      response.headers['Expires'] = '0'
-      render json: users
-    end
+  before_action :authenticate_user! # Deviseの場合、ログイン済みユーザーの認証を行う
+
+  def show
+    user = current_user
+    response.headers['Cache-Control'] = 'no-cache, no-store'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    render json: user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: [:sessions]
-
+  devise_for :users, skip: [:sessions, :registrations]
+  
+  # 新規登録用のルーティングを追加
+  devise_scope :user do
+    post '/sign_up', to: 'api/registrations#create'
+  end
+  
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:index]
+    resources :users, only: [:show]
     devise_scope :user do
       post '/login', to: 'sessions/sessions#create'
     end
