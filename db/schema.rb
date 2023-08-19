7ff7a65dc02ae1329a878ddf1bec0300cdeeb27e
tamaru <on.101.tamaru.taichi@gmail.com>
2023-08-19 23:45:36 +0900

    マイグレーションファイルの変更、データベースをmysqlからpostgreSQLに変更

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,20 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_08_10_064202) do
-  create_table "users", charset: "utf8mb4", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.text "email", default: "", null: false
+    t.text "encrypted_password", default: "", null: false
+    t.text "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
-    t.string "hobbies"
-    t.string "languages"
+    t.text "name"
+    t.text "hobbies"
+    t.text "languages"
     t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
