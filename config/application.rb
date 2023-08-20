f5d053e2eccf217959567f3809b4f6724eff50bf
tamaru <on.101.tamaru.taichi@gmail.com>
2023-08-21 00:49:31 +0900

    オリジンの設定

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,11 @@
+require_relative "boot"
+require "rails/all"
+
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
 module GrowCurriculumBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -5,7 +13,7 @@ module GrowCurriculumBackend
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://grow-official.netlify.app' # Vue.js アプリケーションの Netlify ドメイン
+        origins 'https://grow-official.netlify.app/' # Vue.js アプリケーションの Netlify ドメイン
         resource '*',
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head],
@@ -22,3 +30,4 @@ module GrowCurriculumBackend
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
+
