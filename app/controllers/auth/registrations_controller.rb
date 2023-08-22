e0f443131764eba5ec5039c15b0a2f19a46b6d84
grow-lesson <grow.lesson.info@gmail.com>
2023-08-22 13:46:47 +0900

    不要なキーの削除

--- app/controllers/auth/registrations_controller.rb
+++ app/controllers/auth/registrations_controller.rb
@@ -2,7 +2,7 @@ class Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
   private
 
   def sign_up_params
-    params.permit(:username, :email, :password, :password_confirmation, :last_name_kana, :first_name_kana)
+    params.permit(:username, :email, :password, :last_name_kana, :first_name_kana)
     delete '/auth/sign_out', to: 'sessions#destroy' # ログアウト用のルート
   end
 end
