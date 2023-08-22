b7e39b728863e123c3384c6ca27b8298db9b7883
grow-lesson <grow.lesson.info@gmail.com>
2023-08-22 15:18:55 +0900

    オリジンのドメイン変更

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://grow-curriculum-backend-f10ce9239245.herokuapp.com' # Vue.js アプリケーションの Netlify ドメイン
+    origins 'https://grow-official.netlify.app' # Vue.js アプリケーションの Netlify ドメイン
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
