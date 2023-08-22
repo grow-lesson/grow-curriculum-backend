8d775a00dee5bb82a2c4b450403f17d4c8481d0c
tamaru <on.101.tamaru.taichi@gmail.com>
2023-08-22 23:42:10 +0900

    オリジンの設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,10 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://grow-official.netlify.app' # 正しいオリジンに変更
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: false 
+      credentials: true # クレデンシャルを許可
   end
 end
