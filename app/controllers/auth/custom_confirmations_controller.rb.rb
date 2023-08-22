a2011687aa77ce50110bb4606e582d208dc2962d
tamaru <on.101.tamaru.taichi@gmail.com>
2023-08-23 02:39:43 +0900

    コントローラ名の修正、確認メールのリンク修正

---
+++ app/controllers/auth/custom_confirmations_controller.rb.rb
@@ -0,0 +1,8 @@
+# app/controllers/custom_confirmations_controller.rb
+class CustomConfirmationsController < DeviseTokenAuth::ConfirmationsController
+  protected
+
+  def after_confirmation_path_for(resource_name, resource)
+    "https://grow-official.netlify.app/confirmation-success" # 確認成功時のリダイレクト先のURL
+  end
+end
