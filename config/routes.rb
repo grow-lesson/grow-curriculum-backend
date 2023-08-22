3a1320bbd27fd2464e9e7ac6b08842716ba9d451
grow-lesson <grow.lesson.info@gmail.com>
2023-08-22 11:24:34 +0900

    ルーティングの設定修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
   # Devise Token Authの設定
   mount_devise_token_auth_for 'User', at: 'auth'
-
-  # API用のルーティング（namespaceを追加）
-  namespace :api, defaults: { format: :json } do
-    # Devise Token Authのログイン用ルーティング
-    devise_scope :user do
-      post '/login', to: 'sessions/sessions#create'
-    end
-  end
 end
+
