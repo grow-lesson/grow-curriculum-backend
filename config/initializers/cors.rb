b0318b3e7b6c21bd4872f337bc2f94d92ac730a7
grow-lesson <grow.lesson.info@gmail.com>
2023-08-25 13:46:34 +0900

    エクスポートするカスタムヘッダーを指定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,11 @@
-# config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://grow-official.netlify.app' # 正しいオリジンに変更
+    origins 'https://grow-official.netlify.app'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true # クレデンシャルを許可
+      credentials: true
+      expose: ['access-token', 'client', 'uid'] # エクスポートするカスタムヘッダーを指定
   end
 end
+
