1af315c5b28f5c034114085ad1dff0b0d0aa2755
grow-lesson <grow.lesson.info@gmail.com>
2023-08-25 11:38:30 +0900

    sessionコントローラーの設定変更

--- app/controllers/auth/sessions_controller.rb
+++ app/controllers/auth/sessions_controller.rb
@@ -1,11 +1,12 @@
 class Auth::SessionsController < DeviseTokenAuth::SessionsController
   def create
-    super do |resource|
-      if resource
-        render_login_success(resource)
-      else
-        render_login_error
-      end
+    resource = resource_class.find_by(email: params[:email])
+
+    if resource && resource.valid_password?(params[:password])
+      sign_in(resource_name, resource)
+      render_login_success(resource)
+    else
+      render_login_error
     end
   end
 
