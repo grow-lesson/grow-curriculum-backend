0ce2e1d9aef0ea37c276ebef8b535767884fd4bb
grow-lesson <grow.lesson.info@gmail.com>
2023-08-25 11:38:10 +0900

    不要なコントローラーの削除

--- app/controllers/auth/custom_confirmations_controller.rb
+++
@@ -1,8 +0,0 @@
-# app/controllers/custom_confirmations_controller.rb
-class Auth::CustomConfirmationsController < DeviseTokenAuth::ConfirmationsController
-  protected
-
-  def after_confirmation_path_for(resource_name, resource)
-    "https://grow-official.netlify.app/confirmation-success" # 確認成功時のリダイレクト先のURL
-  end
-end
