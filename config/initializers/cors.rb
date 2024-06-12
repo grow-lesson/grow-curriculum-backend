6c53168cdbcf759a149491b07af84461a57c1139
tamaru <on.101.tamaru.taichi@gmail.com>
2024-06-12 09:37:55 +0900

    CORSのオリジン設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://grow-official.netlify.app'
+    origins 'https://grow-curriculum.com/'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
