0c5d52e48f00f75c9751bd546f71ae5edccb6d5f
tamaru <on.101.tamaru.taichi@gmail.com>
2024-06-12 09:48:05 +0900

    CORSのオリジン設定修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://grow-curriculum.com/'
+    origins 'https://grow-curriculum.com'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
